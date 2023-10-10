323f6c3eb92d4539910f5cd4f0409e99b732f1ec
basil george <basilgeorge0369@gmail.com>
2023-10-10 19:59:45 +0530

    landing page updated

--- script.js
+++ script.js
@@ -2,12 +2,88 @@ const carousel = document.querySelector(".testimonials-content-list");
 const arrowBtns = document.querySelectorAll(".slider-arrow i");
 const firstCardWidth = carousel.querySelector(".testimonial-card").offsetWidth;
 const carouselChildrens = [...carousel.children];
+const totalWidth = carousel.scrollWidth;
 
 // get the number of cards that can fit in the carousal at once
 let cardPerView = Math.random(carousel.offsetWidth / firstCardWidth);
+let cardCount = 3;
 
 arrowBtns.forEach(btn => {
     btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id === "left" ? -(firstCardWidth + 30) : (firstCardWidth + 30);
+        if(cardCount == 9 && btn.id === "right"){
+            carousel.scrollLeft = 0;
+            cardCount = 3;
+        }else{
+            if(btn.id === "left" && cardCount == 3){
+                carousel.scrollLeft = totalWidth;
+                cardCount = 9;
+            }else{
+                btn.id === "left" ? cardCount--: cardCount++;
+                carousel.scrollLeft += btn.id === "left" ? -(firstCardWidth + 30) : (firstCardWidth + 30);
+            }
+        }
     });
+});
+
+const intervalId = setInterval(function() {
+    if(cardCount == 9){
+        carousel.scrollLeft = 0;
+        cardCount = 3;
+    }else {
+        cardCount++;
+        carousel.scrollLeft += firstCardWidth + 30;
+    }
+}, 5000);
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("callback_form");
+    
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+
+        // Get form data
+        const formData = new FormData(event.target);
+
+        const data = {
+            name: formData.get('name'),
+            contact_number: formData.get('contact_number')
+        }
+        
+        // Make an AJAX request using the fetch API
+        fetch('http://localhost:8800/api/lead/save-lead', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response data
+            if(data.status == 200){
+                const inputName = document.getElementById('name');
+                const inputNumber = document.getElementById('contact_number');
+                const successElement = document.getElementById('success-message');
+
+                // Set the display property to "block"
+                successElement.style.display = 'block';
+                inputName.value = '';
+                inputNumber.value = '';
+                setTimeout(function() {
+                    successElement.style.display = 'none';
+                }, 2000);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    });
+});
+
+document.getElementById('phoneNumber').addEventListener('click', function() {
+    window.location.href = 'tel:'+document.getElementById('phoneNumber').value;
+});
+
+document.getElementById('phoneNumber_bm').addEventListener('click', function() {
+    window.location.href = 'tel:'+document.getElementById('phoneNumber').value;
 });
