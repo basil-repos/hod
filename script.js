d9a13346d5bc6684b6077f0db9192bf72185df4f
basil george <basilgeorge0369@gmail.com>
2023-10-31 20:38:10 +0530

    landing page updated

--- script.js
+++ script.js
@@ -39,15 +39,23 @@ document.addEventListener("DOMContentLoaded", function () {
     const form = document.getElementById("callback_form");
     const button = document.querySelector('.submit-btn');
     const loaderIcon = document.querySelector('.loader-icon');
+    const input = document.getElementById("contact_number");
+    const errorMessage = document.getElementById("error-message");
     
     form.addEventListener("submit", function (e) {
         e.preventDefault();
-        
-        button.disabled = true;
-        loaderIcon.style.display = "block";
 
         // Get form data
         const formData = new FormData(event.target);
+
+        if (input.value.length != 10) {
+            errorMessage.textContent = "Mobile number must be 10 digits long";
+            e.preventDefault();
+            return;
+        }
+
+        button.disabled = true;
+        loaderIcon.style.display = "block";
 
         const data = {
             name: formData.get('name'),
@@ -55,34 +63,32 @@ document.addEventListener("DOMContentLoaded", function () {
         }
         
         // Make an AJAX request using the fetch API
-        fetch('https://api.artistrymedia.agency/save-lead', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(data)
-        })
-        .then(response => response.json())
-        .then(data => {
-            // Handle the response data
-            if(data.status == "Success"){
+        // fetch('https://api.artistrymedia.agency/save-lead', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //     },
+        //     body: JSON.stringify(data)
+        // })
+        // .then(response => response.json())
+        // .then(data => {
+        //     // Handle the response data
+        //     if(data.status == "Success"){
                 const inputName = document.getElementById('name');
                 const inputNumber = document.getElementById('contact_number');
                 const successElement = document.getElementById('success-message');
+                const formElement = document.getElementById('callback_form');
                 button.disabled = false;
                 loaderIcon.style.display = "none";
-                successElement.style.display = 'block';
+                formElement.style.display = 'none';
+                successElement.style.display = 'flex';
                 inputName.value = '';
                 inputNumber.value = '';
-                
-                setTimeout(function() {
-                    successElement.style.display = 'none';
-                }, 2000);
-            }
-        })
-        .catch(error => {
-            console.error('Error:', error);
-        });
+            // }
+        // })
+        // .catch(error => {
+        //     console.error('Error:', error);
+        // });
     });
 
     const scrollButton = document.getElementById("callback_btn");
@@ -92,6 +98,34 @@ document.addEventListener("DOMContentLoaded", function () {
         targetDiv.scrollIntoView({
             behavior: "smooth"
         })
+    });
+
+    document.querySelector('#contact_number').addEventListener('input', function(event) {
+        const inputElement = event.target;
+        const inputValue = event.target.value;
+        
+        const sanitizedValue = inputValue.replace(/[^0-9]/g, '');
+        inputElement.value = sanitizedValue;
+
+        if (input.value.length == 10) {
+            errorMessage.textContent = "";
+        }
+    });
+    
+    const newSubmitElement = document.getElementById("new-submit");
+    newSubmitElement.addEventListener("click", function() {
+        const successElement = document.getElementById('success-message');
+        const formElement = document.getElementById('callback_form');
+        successElement.style.display = 'none';
+        formElement.style.display = 'flex';
+    });
+    
+    document.querySelector('#name').addEventListener('input', function(event) {
+        const inputElement = event.target;
+        const inputValue = event.target.value;
+        
+        const sanitizedValue = inputValue.replace(/[^a-zA-Z .'" ]/g, '');
+        inputElement.value = sanitizedValue;
     });
 });
 
